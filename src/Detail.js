2a7c16ffab2c01d29d81e80cede5ec54aec2dcd2
SEUNGMIN KIM <smk950226@naver.com>
2019-04-01 03:47:52 +0900

    query with variable, end without last css

--- src/Detail.js
+++ src/Detail.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Query } from "react-apollo";
+import { MOVIE_DETAILS } from './queries';
 
 const Detail = ({
     match: {
       params: { movieId }
     }
-  }) => {
-    console.log(movieId);
-    return <div>Detail</div>;
-  };
+  }) => <Query query={MOVIE_DETAILS} variables={{movieId: parseInt(movieId)}}>{({ loading, data, error }) => {
+      if(loading) return "loading";
+      if(error) return "error";
+      return (
+        <React.Fragment>
+            {data.movie.title}
+            {data.movie.medium_cover_image}
+            {data.movie.rating}
+            {data.movie.description_intro}
+        </React.Fragment>
+      )
+  }}</Query>
 
 export default Detail;
